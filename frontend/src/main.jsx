09d03783d2c36dd20b691fc8007deeae556c1fa6
alan-desing <alanbissio@gmail.com>
2026-06-02 21:29:23 -0300

    frontend conectado al gateway

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink
+} from '@apollo/client'
+
+import { ApolloProvider } from '@apollo/client/react'
+
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  }),
+  cache: new InMemoryCache()
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+  </ApolloProvider>
 )
